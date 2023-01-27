b9fd4786c0522da685860e12d334ffd646dbd842
dimon_nich <dimon_nich@mail.ru>
2023-01-27 21:08:21 +0300

    Final program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array1 = new string[5] {"156", "-2", "Denmark", "Russia", "res"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+    Console.WriteLine();
